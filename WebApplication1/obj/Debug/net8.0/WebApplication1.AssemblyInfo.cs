a16f54129efddc8c23a0b5ebed9a2cb7fa9b2115
DikshaSoni705 <sonidiksha1602@gmail.com>
2025-12-03 14:48:47 +0530

    logggo

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af67084a457cadbfc222200cc25bda39a3b15bfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bff2ad614d96004877476a363a269fa82c67714e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
