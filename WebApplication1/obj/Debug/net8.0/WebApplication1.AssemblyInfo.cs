af67084a457cadbfc222200cc25bda39a3b15bfa
DikshaSoni705 <sonidiksha1602@gmail.com>
2025-12-03 14:43:46 +0530

    uiiiiii

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99abad51201da271e942ec471831a1a11ce27bb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d44c4c5dd07ac94e43283df2c58bc03a537d7eac")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
